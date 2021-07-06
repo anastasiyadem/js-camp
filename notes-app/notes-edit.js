00ba19d925b89c1de37266e960b15b7232cea2cc
Anastasiia Demchenko <demchenko.anastasia@gmail.com>
2021-07-06 11:58:22 +0200

    set up edit paje for note editing

--- notes-app/notes-edit.js
+++ notes-app/notes-edit.js
@@ -1,3 +1,6 @@
+const titleElement = document.querySelector('#note-title')
+const bodyElement = document.querySelector('#note-body')
+const removeElement = document.querySelector('#remove-note')
 const noteId = location.hash.substring(1)
 const notes = getSavedNotes()
 const note = notes.find(function(note) {
@@ -7,6 +10,25 @@ const note = notes.find(function(note) {
 if (note === undefined) {
     location.assign('/index.html')
 }
+titleElement.value = note.title
+bodyElement.value = note.body
 
-document.querySelector('#note-title').value = note.title
-document.querySelector('#note-body').value = note.body
+titleElement.addEventListener('input', function(e) {
+    note.title = e.target.value
+    saveNotes(notes)
+})
+bodyElement.addEventListener('input', function(e) {
+    note.body = e.target.value
+    saveNotes(notes)
+})
+removeElement.addEventListener('click', function(e) {
+    removeNote(note.id)
+    saveNotes(notes)
+    location.assign('/index.html')
+})
+
+document.querySelector('#save-note').addEventListener('click', function (e) {
+    saveNotes(notes)
+    location.assign('/index.html')
+})
+
