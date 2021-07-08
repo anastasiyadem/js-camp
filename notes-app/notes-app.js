3ee41736aa70ae63faf00959cab37e7308edd623
Anastasiia Demchenko <demchenko.anastasia@gmail.com>
2021-07-08 11:25:46 +0200

    adding date info in js

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -34,3 +34,34 @@ window.addEventListener('storage', function(e) {
         renderNotes(notes, filters)   
     }
 })
+
+//Unix Epoch - January 1st 1970 00:00:00
+const now = new Date()
+const timestamp = now.getTime()
+
+const myDate = new Date(timestamp)
+console.log(myDate.getFullYear())
+
+const past1 = new Date('March 18 2020 7:44:51')
+const past2 = new Date('June 6 2020 12:21:03')
+const timePast1 = past1.getTime()
+const timePast2 = past2.getTime()
+
+if (timePast1 < timePast2) {
+    console.log(past1.toString())
+} else if (timePast2 < timePast1) {
+    console.log(past2.toString())
+}
+
+// console.log(now.toString())
+
+// console.log(`Year: ${now.getFullYear()}`)
+// console.log(`Month: ${now.getMonth()}`)
+// console.log(`Day of Month: ${now.getDate()}`)
+// console.log(`Hour: ${now.getHours()}`)
+// console.log(`Minutes: ${now.getMinutes()}`)
+// console.log(`Seconds: ${now.getSeconds()}`)
+
+//1.Create two dates in the past(use strings for dates)
+//2.Get timestamps for both.
+//3.Figure out which is first and print its time(use toString)
